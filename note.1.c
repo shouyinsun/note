22efdd0ce7edaf90548f38a4e7bf25aaad96e552
cash_xu <xuchaoshanghai@163.com>
2019-07-16 11:07:26 +0800

    Update note.1.c

--- note.1.c
+++ note.1.c
@@ -772,3 +772,38 @@ select  AES_DECRYPT(FROM_BASE64(SUBSTRING(mobile,5)),'1fi;qPa7utddahWy')  FROM `
 
     ### idea lombok 找不到方法错误
     https://blog.csdn.net/xzp_12345/article/details/80268834
+    
+    
+    ### lua 脚本,Lua是从1开始排序
+    ###redis 抢红包
+	-- 脚本：尝试获得红包,如果成功,则返回json字符串,如果不成功,则返回空
+	-- 参数：红包队列名 hongBaoList, 已消费的队列名 hongBaoConsumedList,去重的Map名 hongBaoConsumedMap,用户ID
+	-- 返回值：nil 或者 json字符串,包含用户ID：userId,红包ID：id,红包金额：money
+	--  jedis.eval(getScript(), 4, hongBaoList, hongBaoConsumedList, hongBaoConsumedMap, "" + j)
+	if redis.call('hexists', KEYS[3], KEYS[4]) ~= 0 then
+	      return nil
+	      else
+	      -- 先取出一个小红包
+	      local hongBao = redis.call('rpop', KEYS[1])
+	      -- hongbao ： {"Money":9,"Id":8}
+	          if hongBao then
+	              local x = cjson.decode(hongBao)
+	              -- 加入用户ID信息
+	              x['userId'] = KEYS[4]
+	              local re = cjson.encode(x)
+	              -- 把用户ID放到去重的set里
+	              redis.call('hset', KEYS[3], KEYS[4], KEYS[4])
+	              -- 把红包放到已消费队列里
+	              redis.call('lpush', KEYS[2], re)
+	          return re;
+	          end
+	      end
+	return nil
+
+
+	### script load 脚本缓存,通过脚本内容的SHA1摘要来执行脚本
+	redis 127.0.0.1:6379> SCRIPT LOAD "return 'hello moto'"
+	"232fd51614574cf0867b83d384a5e898cfd24e5a"
+ 
+	redis 127.0.0.1:6379> EVALSHA "232fd51614574cf0867b83d384a5e898cfd24e5a" 0
+	"hello moto"
